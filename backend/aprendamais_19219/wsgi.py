0abd5e3046313d3fd9525db74f2c4039b07cc3a9
Crowdbotics <team@crowdbotics.com>
2020-07-28 16:37:41 +0000

    Update models

--- backend/aprendamais_19219/wsgi.py
+++ backend/aprendamais_19219/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aprendamais_19219.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aprendamais_19219.settings")
 
 application = get_wsgi_application()
